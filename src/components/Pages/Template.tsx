efce3ed0e3cb8672018e617b0393ca6e457f7cc5
adtak <adtak62@gmail.com>
2023-09-02 00:27:36 +0900

    add Template

---
+++ src/components/Pages/Template.tsx
@@ -0,0 +1,237 @@
+import * as React from "react";
+import clsx from "clsx";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import {
+  AppBar,
+  Drawer,
+  Toolbar,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  Theme,
+  createStyles,
+  colors,
+  IconButton,
+  Typography,
+  Divider,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Container,
+  Box,
+  makeStyles,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+
+const drawerWidth = 240;
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Noto Sans JP",
+      "Lato",
+      "游ゴシック Medium",
+      "游ゴシック体",
+      "Yu Gothic Medium",
+      "YuGothic",
+      "ヒラギノ角ゴ ProN",
+      "Hiragino Kaku Gothic ProN",
+      "メイリオ",
+      "Meiryo",
+      "ＭＳ Ｐゴシック",
+      "MS PGothic",
+      "sans-serif",
+    ].join(","),
+  },
+  palette: {
+    primary: { main: colors.blue[800] },
+  },
+});
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    toolbar: {
+      paddingRight: 24,
+    },
+    toolbarIcon: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: "0 8px",
+      ...theme.mixins.toolbar,
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginRight: 36,
+    },
+    menuButtonHidden: {
+      display: "none",
+    },
+    title: {
+      flexGrow: 1,
+    },
+    pageTitle: {
+      marginBottom: theme.spacing(1),
+    },
+    drawerPaper: {
+      position: "relative",
+      whiteSpace: "nowrap",
+      width: drawerWidth,
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerPaperClose: {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      height: "100vh",
+      overflow: "auto",
+    },
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      display: "flex",
+      overflow: "auto",
+      flexDirection: "column",
+    },
+    link: {
+      textDecoration: "none",
+      color: theme.palette.text.secondary,
+    },
+  })
+);
+
+export interface TemplateProps {
+  children: React.ReactNode;
+  title: string;
+}
+
+export default function Template(props: TemplateProps) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(true);
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position="absolute"
+          className={clsx(classes.appBar, open && classes.appBarShift)}
+        >
+          <Toolbar className={classes.toolbar}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              className={clsx(
+                classes.menuButton,
+                open && classes.menuButtonHidden
+              )}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              className={classes.title}
+            >
+              管理画面
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="permanent"
+          classes={{
+            paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+          }}
+          open={open}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+          </div>
+          <Divider />
+          <List>
+            <Link to="/" className={classes.link}>
+              <ListItem>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="トップページ" />
+              </ListItem>
+            </Link>
+            <Link to="/products" className={classes.link}>
+              <ListItem>
+                <ListItemIcon>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="商品ページ" />
+              </ListItem>
+            </Link>
+          </List>
+        </Drawer>
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Container maxWidth="lg" className={classes.container}>
+            <Typography
+              component="h2"
+              variant="h5"
+              color="inherit"
+              noWrap
+              className={classes.pageTitle}
+            >
+              {props.title}
+            </Typography>
+            {props.children}
+            <Box pt={4}></Box>
+          </Container>
+        </main>
+      </div>
+    </ThemeProvider>
+  );
+}
