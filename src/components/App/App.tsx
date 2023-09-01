f69b190ff99e67e64367beb1bc7363f993f45200
adtak <adtak62@gmail.com>
2023-09-01 22:55:56 +0900

    move App

---
+++ src/components/App/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>
+    },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  )
+}
+
+export default App
